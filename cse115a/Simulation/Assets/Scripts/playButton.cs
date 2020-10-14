50aaf2dde35758e79f3b14c8a3b2cb0d5c0bdffb
Victor <hkchung@ucsc.edu>
2020-10-13 20:19:55 -0700

    Project Pushed

---
+++ cse115a/Simulation/Assets/Scripts/playButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playButton : MonoBehaviour
+{
+	public Animator transition;
+	public float transitionTime = 1f;
+
+    public void startGame()
+	{
+		StartCoroutine(loadLevel());
+	}
+
+	IEnumerator loadLevel()
+	{
+		transition.SetTrigger("Start");
+
+		//wait
+		yield return new WaitForSeconds(transitionTime);
+
+		SceneManager.LoadScene("main");
+	}
+}
